7ed466af5dfcccef7c766d9b30ff1853671ee28c
jingchu.fhb <jingchu.fhb@alibaba-inc.com>
2016-08-03 09:03:39 +0800

    neteasy exam

---
+++ cc.js
@@ -0,0 +1,59 @@
+// var str = 'data';
+// var i;
+// var j;
+// var str1;
+// var result = [];
+// var arr;
+// var temp;
+// var ctemp;
+// for (i = 0; i < str.length; i++) {
+//     arr = str.split('');
+//     arr.splice(i, 1);
+//     for (j = 0; j < arr.length; j++) {
+//         temp = arr.concat([]);
+//         temp.splice(j, 1);
+//         ctemp = temp.join('');
+//         if (result.every(function(val, index, arr) {
+//                 if (ctemp === val) {
+//                     return false;
+//                 }
+//                 else {
+//                     return true;
+//                 }
+//             })) {
+//             result.push(ctemp.toString());
+//         }
+//     }
+// }
+//
+// console.info(result);
+// console.info(result.length);
+//
+//
+//
+//
+//
+
+// function over(h) {
+//     if (h < 1 || h > 10e18) {
+//         return false;
+//     }
+//     var result;
+//     for (var i = 0; i < h; i++) {
+//         if (i + i * i > h) {
+//             console.info(i - 1);
+//             result = i - 1;
+//             break;
+//         }
+//     }
+//     return result;
+// }
+// over(13);
+//
+//
+console.log(0==="");
+console.log(0==="0");
+console.log(null===undefined);
+console.log(false==="false");
+console.log(0==="");
+console.log(false==="0");
